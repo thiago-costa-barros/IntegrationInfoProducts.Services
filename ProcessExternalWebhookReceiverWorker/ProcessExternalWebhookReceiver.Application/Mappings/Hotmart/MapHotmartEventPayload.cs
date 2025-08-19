d0fdcdffb6fd305a6cca0e78f741a129ed084d0f
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-19 08:28:28 -0300

    feat: add mapHotmartEventPayload

---
+++ ProcessExternalWebhookReceiverWorker/ProcessExternalWebhookReceiver.Application/Mappings/Hotmart/MapHotmartEventPayload.cs
@@ -0,0 +1,33 @@
+﻿using CommonSolution.Entities.IntegrationSchema;
+using CommonSolution.Extensions;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events.Objects.HotmartPurchaseEvent;
+
+namespace ProcessExternalWebhookReceiver.Application.Mappings.Hotmart
+{
+    public class MapHotmartEventPayload
+    {
+        public static Task<HotmartEventPayload<HotmartPurchaseEventObjectPayment>> MapHotmartPurchaseEventPayload(ExternalWebhookReceiver externalWebhookReceiver,HotmartWebhookReceiverPayload hotmartWebhookReceiverPayload) 
+        {
+            HotmartEventPayload<HotmartPurchaseEventObjectPayment> hotmartEventPayload = new HotmartEventPayload<HotmartPurchaseEventObjectPayment>
+            {
+                ExternalWebhookReceiverId = externalWebhookReceiver.ExternalWebhookReceiverId,
+                SourceType = externalWebhookReceiver.SourceType,
+                Status = externalWebhookReceiver.Status,
+                CompanyId = externalWebhookReceiver.CompanyId,
+                ExternalIdentifier = externalWebhookReceiver.ExternalIdentifier,
+                Payload = new HotmartEventPayloadData<HotmartPurchaseEventObjectPayment>
+                {
+                    Id = hotmartWebhookReceiverPayload.Id,
+                    CreationDate = hotmartWebhookReceiverPayload.CreationDate,
+                    Event = hotmartWebhookReceiverPayload.Event,
+                    Version = hotmartWebhookReceiverPayload.Version,
+                    Data = hotmartWebhookReceiverPayload.Data.MapToObject<HotmartPurchaseEventObjectPayment>()
+                }
+            };
+
+            return Task.FromResult(hotmartEventPayload);
+        }
+    }
+}
