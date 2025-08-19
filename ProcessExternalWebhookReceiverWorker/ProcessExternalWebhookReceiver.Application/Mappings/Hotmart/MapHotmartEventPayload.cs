18d8038385b2ba01a50a712b1110874cf0bf9f2c
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-19 08:51:52 -0300

    fix: ajuste no MapHotmartPurchaseEventPayload para a classe correta

--- ProcessExternalWebhookReceiverWorker/ProcessExternalWebhookReceiver.Application/Mappings/Hotmart/MapHotmartEventPayload.cs
+++ ProcessExternalWebhookReceiverWorker/ProcessExternalWebhookReceiver.Application/Mappings/Hotmart/MapHotmartEventPayload.cs
@@ -2,28 +2,28 @@
 using CommonSolution.Extensions;
 using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart;
 using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events;
-using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events.Objects.HotmartPurchaseEvent;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events.Objects;
 
 namespace ProcessExternalWebhookReceiver.Application.Mappings.Hotmart
 {
     public class MapHotmartEventPayload
     {
-        public static Task<HotmartEventPayload<HotmartPurchaseEventObjectPayment>> MapHotmartPurchaseEventPayload(ExternalWebhookReceiver externalWebhookReceiver,HotmartWebhookReceiverPayload hotmartWebhookReceiverPayload) 
+        public static Task<HotmartEventPayload<HotmartPuchaseEventPayload>> MapHotmartPurchaseEventPayload(ExternalWebhookReceiver externalWebhookReceiver,HotmartWebhookReceiverPayload hotmartWebhookReceiverPayload) 
         {
-            HotmartEventPayload<HotmartPurchaseEventObjectPayment> hotmartEventPayload = new HotmartEventPayload<HotmartPurchaseEventObjectPayment>
+            HotmartEventPayload<HotmartPuchaseEventPayload> hotmartEventPayload = new HotmartEventPayload<HotmartPuchaseEventPayload>
             {
                 ExternalWebhookReceiverId = externalWebhookReceiver.ExternalWebhookReceiverId,
                 SourceType = externalWebhookReceiver.SourceType,
                 Status = externalWebhookReceiver.Status,
                 CompanyId = externalWebhookReceiver.CompanyId,
                 ExternalIdentifier = externalWebhookReceiver.ExternalIdentifier,
-                Payload = new HotmartEventPayloadData<HotmartPurchaseEventObjectPayment>
+                Payload = new HotmartEventPayloadData<HotmartPuchaseEventPayload>
                 {
                     Id = hotmartWebhookReceiverPayload.Id,
                     CreationDate = hotmartWebhookReceiverPayload.CreationDate,
                     Event = hotmartWebhookReceiverPayload.Event,
                     Version = hotmartWebhookReceiverPayload.Version,
-                    Data = hotmartWebhookReceiverPayload.Data.MapToObject<HotmartPurchaseEventObjectPayment>()
+                    Data = hotmartWebhookReceiverPayload.Data.MapToObject<HotmartPuchaseEventPayload>()
                 }
             };
 
