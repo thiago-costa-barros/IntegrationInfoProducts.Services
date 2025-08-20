60fdd8c4b38bb4141a8bb8d9be4168131183401e
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-20 07:44:55 -0300

    add: hotmarteventpucharse service

---
+++ ProcessExternalWebhookReceiverWorker/ProcessExternalWebhookReceiver.Application/Interfaces/Services/Hotmart/Events/IHotmartEventPurchaseService.cs
@@ -0,0 +1,19 @@
+﻿using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events.Objects;
+
+namespace ProcessExternalWebhookReceiver.Application.Interfaces.Services.Hotmart.Events
+{
+    public interface IHotmartEventPurchaseService
+    {
+        Task HandlePurchaseEventsAsync(HotmartEventPayload<HotmartPuchaseEventPayload> hotmartEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseApprovedAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseCanceledAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseCompleteAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseBilletPrintedAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseProtestAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseRefundedAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseChargebackAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseExpiredAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+        //Task HandlePurchaseDelayedAsync(HotmartPuchaseEventPayload hotmartPuchaseEventPayload, CancellationToken cancellationToken);
+    }
+}
---
+++ ProcessExternalWebhookReceiverWorker/ProcessExternalWebhookReceiver.Application/Services/Hotmart/Events/HotmartEventPurchaseService.cs
@@ -0,0 +1,19 @@
+﻿using CommonSolution.Entities.CoreSchema;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events;
+using ProcessExternalWebhookReceiver.Application.DTOs.Hotmart.Events.Objects;
+using ProcessExternalWebhookReceiver.Application.Interfaces.Services.Hotmart.Events;
+
+namespace ProcessExternalWebhookReceiver.Application.Services.Hotmart.Events
+{
+    public class HotmartEventPurchaseService : IHotmartEventPurchaseService
+    {
+        public async Task HandlePurchaseEventsAsync(HotmartEventPayload<HotmartPuchaseEventPayload> hotmartEventPayload, CancellationToken cancellationToken)
+        {
+            string personTaxNumber = hotmartEventPayload.Payload?.Data.Producer?.Document;
+            if (string.IsNullOrEmpty(personTaxNumber))
+                throw new InvalidOperationException("O número de documento do produtor não pode ser nulo ou vazio.");
+            //continuar com gets de company, companybranch e person
+
+        }
+    }
+}
